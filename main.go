6531c22f4b1ac23627fca159bac70b752bb894c9
heloise <37453877+heloise-viegas@users.noreply.github.com>
2022-05-21 22:15:11 +0530

    Update main.go

--- main.go
+++ main.go
@@ -15,12 +15,12 @@ type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
 	Title    string    `json:"title"`
-	Director *Director `json:"director`
+	Director *Director `json:"director"`
 }
 
 type Director struct {
 	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname`
+	Lastname  string `json:"lastname"`
 }
 
 var movies []Movie
